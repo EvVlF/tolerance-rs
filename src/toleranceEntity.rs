eeba0fa99bb89d79c7c5d56fe873f8859327c5b5
EvVlF <ev.vl.fedorov@gmail.com>
2024-06-05 21:47:31 +0400

    add value: ToleranceValue field in struct ToleranceEntity

--- src/toleranceEntity.rs
+++ src/toleranceEntity.rs
@@ -1,8 +1,10 @@
 use crate::accuracy::Accuracy;
 use crate::toleranceField::ToleranceField;
+use crate::toleranceValue::ToleranceValue;
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
 struct ToleranceEntity {
     field: ToleranceField,
     grade: Accuracy,
+    value: ToleranceValue,
 }

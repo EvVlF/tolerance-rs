11569401498966a0963b8b2dd04beada318a9eac
EvVlF <ev.vl.fedorov@gmail.com>
2024-07-10 21:35:11 +0400

    refactor tolerance_table for r2d2 pools

--- src/tolerance_table.rs
+++ src/tolerance_table.rs
@@ -1,31 +1,39 @@
-use std::collections::HashMap;
-use lazy_static::lazy_static;
-use rust_decimal::Decimal;
-use crate::accuracy::Accuracy;
-use crate::size_range::SizeRange;
-use crate::tolerance_accuracy::ToleranceAccuracy;
-use crate::tolerance_field::{ToleranceField, TolShaft};
-use crate::tolerance_value::ToleranceValue;
+use std::fs;
+use std::io::Read;
+use std::path::Path;
+use r2d2::Pool;
+use r2d2_sqlite::SqliteConnectionManager;
 
-type SizeToleranceAccuracy = (SizeRange, ToleranceAccuracy);
-type ToleranceTable = HashMap<SizeToleranceAccuracy, ToleranceValue>;
 
-lazy_static! {
-pub static ref TOLERANCE_TABLE: ToleranceTable = initialize_tolerance_table();
+pub struct ToleranceTable {
+    pub pool: Pool<SqliteConnectionManager>,
+}
+
+impl ToleranceTable {
+    pub fn new() -> Self {
+        ToleranceTable { pool: Self::init_pool() }
     }
+}
 
-fn initialize_tolerance_table() -> ToleranceTable {
-    let mut table: ToleranceTable = HashMap::new();
+trait ToleranceTablePool {
+    fn init_pool() -> Pool<SqliteConnectionManager>;
+}
 
-    table.insert(
-        (
-            SizeRange { min: Decimal::from(1), max: Decimal::from(3) },
-            ToleranceAccuracy {
-                tolerance: ToleranceField::Shaft(TolShaft::g),
-                accuracy: Accuracy::Class5,
-            },
-        ),
-        ToleranceValue { upper: Decimal::from(-2), lower: Decimal::from(-6) },
-    );
-    table
+impl ToleranceTablePool for ToleranceTable {
+    fn init_pool() -> Pool<SqliteConnectionManager> {
+        let manager = SqliteConnectionManager::memory();
+        let pool = Pool::new(manager).expect("Не удалось создать пул соединений");
+        {
+            let conn = pool.get().expect("Не удалось получить соединение из пула");
+            let path = Path::new("data/dump.sql");
+            let mut file = fs::File::open(&path).expect("Не удалось открыть файл БД");
+            let mut dump = String::new();
+            file.read_to_string(&mut dump)
+                .expect("Не удалось прочитать файл БД");
+            conn.execute_batch(&dump)
+                .expect("Не удалось развернуть БД из dump.sql");
+        }
+
+        pool
+    }
 }

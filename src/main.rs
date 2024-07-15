c70e4e0c692e29978f755ad8730b4c7c6ef189c1
EvVlF <ev.vl.fedorov@gmail.com>
2024-07-15 20:24:18 +0400

    prep impl find tolerance in one table

--- src/main.rs
+++ src/main.rs
@@ -1,73 +1,68 @@
-use std::io::{self, Write};
-use std::collections::HashMap;
-use std::str::FromStr;
-use lazy_static::lazy_static;
-use regex::Regex;
-use rust_decimal::Decimal;
-use crate::accuracy::Accuracy;
-use crate::size_range::SizeRange;
-use crate::tolerance_accuracy::ToleranceAccuracy;
-use crate::tolerance_field::{ToleranceField, TolHole, TolShaft};
-use crate::tolerance_value::ToleranceValue;
+extern crate alloc;
 
-mod tolerance_field;
+use crate::tolerance_field::{ToleranceField};
+use alloc::boxed::Box;
+use alloc::string::String;
+use regex::Regex;
+use rusqlite::{Result};
+use rust_decimal::Decimal;
+use std::error::Error;
+use std::io::{self, Read, Write};
+use std::str::FromStr;
+use std::sync::{Arc};
+use lazy_static::lazy_static;
+use r2d2::Pool;
+use r2d2_sqlite::SqliteConnectionManager;
+use rayon::prelude::*;
+use crate::tolerance_table::{ToleranceTable};
+
 mod accuracy;
 mod size_range;
 mod tolerance_accuracy;
 mod tolerance_entity;
+mod tolerance_field;
 mod tolerance_value;
-
-type SizeToleranceAccuracy = (SizeRange, ToleranceAccuracy);
-type ToleranceTable = HashMap<SizeToleranceAccuracy, ToleranceValue>;
+mod tolerance_table;
 
 lazy_static! {
-static ref TOLERANCE_TABLE: ToleranceTable = initialize_tolerance_table();
-    }
+    static ref POOL: Arc<Pool<SqliteConnectionManager>> = Arc::new(ToleranceTable::new().pool);
+}
 
 fn main() {
-    loop {
-        if let Some((size, field, accuracy)) = get_input() {
-            if let Some(tolerance_value) = find_tolerance_entry(size, field, accuracy) {
-                print_result(size, tolerance_value);
-            } else {
-                println!("Значение не найдено");
-            }
-        } else {
-            println!("Неверный формат ввода. Пожалуйста, попробуйте снова.");
-        }
-    }
+    search_in_table(&POOL, "H6", "ISO_Hole_Limits_H_JS", 90).expect("ooops");
+    // loop {
+    //     if let Some((size, field, accuracy)) = get_input() {
+    //         if let Some(tolerance_value) = find_tolerance_entry(size, field, accuracy) {
+    //             print_result(size, tolerance_value);
+    //         } else {
+    //             println!("Значение не найдено");
+    //         }
+    //     } else {
+    //         println!("Неверный формат ввода. Пожалуйста, попробуйте снова.");
+    //     }
+    // }
 }
 
-fn initialize_tolerance_table() -> ToleranceTable {
-    let mut table: ToleranceTable = HashMap::new();
+// fn search_in_tables(pool: &Arc<Pool<SqliteConnectionManager>>, search_value: String) -> Result<(), Box<dyn Error>> {
+//     let search_table_ids = vec!["ISO_Hole_Limits_A_C", "ISO_Hole_Limits_CD_G", "ISO_Hole_Limits_H_JS", "ISO_Hole_Limits_J_P", "ISO_Hole_Limits_R_S", "ISO_Hole_Limits_T_X", "ISO_Hole_Limits_Z_ZC", "ISO_Shaft_Limits_a_c", "ISO_Shaft_Limits_cd_g", "ISO_Shaft_Limits_h_js", "ISO_Shaft_Limits_j_p", "ISO_Shaft_Limits_r_s", "ISO_Shaft_Limits_t_u", "ISO_Shaft_Limits_v_zc", "AI_Shaft_Limits_a_js"];
+//
+// }
 
-    table.insert(
-        (
-            SizeRange { min: Decimal::from(1), max: Decimal::from(3) },
-            ToleranceAccuracy {
-                tolerance: ToleranceField::Shaft(TolShaft::g),
-                accuracy: Accuracy::Class5,
-            },
-        ),
-        ToleranceValue { upper: Decimal::from(-2), lower: Decimal::from(-6) },
-    );
-    table
-}
+fn search_in_table(pool: &Arc<Pool<SqliteConnectionManager>>, tol_field_accuracy: &str, table_name: &str, dia: u16) -> Result<(), Box<dyn Error>> {
+    let connection = pool.get().expect("Не удалось получить пул соединения БД");
+    let query = format!("SELECT {} FROM {} WHERE MIN_DIA <= {2} AND MAX_DIA > {2}", tol_field_accuracy, table_name, dia);
+    let mut stmt = connection.prepare(&query)?;
 
-fn find_tolerance_entry(size: Decimal, field: ToleranceField, accuracy: u8) -> Option<ToleranceValue> {
-    if let Some(accuracy) = Accuracy::match_accuracy(accuracy) {
-        let key = ToleranceAccuracy {
-            tolerance: field,
-            accuracy,
-        };
+    let mut rows = stmt.query_map([], |row| {
+        let tol: String = row.get(0)?;
 
-        for ((size_range, tolerance_accuracy), value) in TOLERANCE_TABLE.iter() {
-            if size > size_range.min && size <= size_range.max && *tolerance_accuracy == key {
-                return Some(value.clone());
-            }
-        }
-    }
-    None
+        Ok(tol)
+    })?;
+    let min_tol = rows.next().unwrap()?;
+    let max_tol = rows.next().unwrap()?;
+    println!("Min Tol: {}, \nMax Tol: {}", min_tol, max_tol);
+
+    Ok(())
 }
 
 fn get_input() -> Option<(Decimal, ToleranceField, u8)> {
@@ -80,13 +75,15 @@ fn get_input() -> Option<(Decimal, ToleranceField, u8)> {
 
 fn read_input() -> String {
     let mut input = String::new();
-    io::stdin().read_line(&mut input).expect("Ошибка при чтении строки");
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Ошибка при чтении строки");
     input
 }
 
 fn parse_input(input: &str) -> Option<(Decimal, ToleranceField, u8)> {
-    let re = Regex::new(r"^(\d{1,3})([a-zA-Z]+)(\d{1,2})$").unwrap();
-    if let Some(captures) = re.captures(input) {
+    let regex = Regex::new(r"^(\d{1,3})([a-zA-Z]+)(\d{1,2})$").unwrap();
+    if let Some(captures) = regex.captures(input) {
         let size: Decimal = captures[1].parse().ok()?;
         let field_str = &captures[2];
         let accuracy: u8 = captures[3].parse().ok()?;
@@ -102,9 +99,15 @@ fn print_result(size: Decimal, tolerance_value: tolerance_value::ToleranceValue)
     let lower_mm = tolerance_value.lower / Decimal::new(1000, 0);
 
     println!("верхнее отклонение:  {:>7} мм", upper_mm);
-    println!("среднее отклонение:  {:>7} мм", (upper_mm + lower_mm) / Decimal::new(2, 0));
+    println!(
+        "среднее отклонение:  {:>7} мм",
+        (upper_mm + lower_mm) / Decimal::new(2, 0)
+    );
     println!("нижнее отклонение:   {:>7} мм", lower_mm);
     println!("максимальный размер: {:>7} мм", size + upper_mm);
-    println!("средний размер:      {:>7} мм", size + (upper_mm + lower_mm) / Decimal::new(2, 0));
+    println!(
+        "средний размер:      {:>7} мм",
+        size + (upper_mm + lower_mm) / Decimal::new(2, 0)
+    );
     println!("минимальный размер:  {:>7} мм", size + lower_mm);
 }
